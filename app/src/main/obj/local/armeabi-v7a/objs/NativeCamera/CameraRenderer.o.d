55d701b642e7c831d35e816f89080939ad50845c
masashiGMS <masashi@gmsnet.com>
2015-02-06 10:09:57 -0800

    initial commit

---
+++ app/src/main/obj/local/armeabi-v7a/objs/NativeCamera/CameraRenderer.o.d
@@ -0,0 +1,353 @@
+/Users/masashi/Documents/workspace/BlogNativeCamera/app/src/main/obj/local/armeabi-v7a/objs/NativeCamera/CameraRenderer.o: \
+ /Users/masashi/Documents/workspace/BlogNativeCamera/app/src/main/jni/CameraRenderer.cpp \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/jni.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/GLES/gl.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/GLES/glplatform.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/GLES/glext.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/pthread.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/time.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/time.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/time.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/siginfo.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/signal.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/limits.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/string.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/signal.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm-generic/signal.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/sigcontext.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/ucontext.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/user.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sched.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/Math.h \
+ sdk/native/jni/include/opencv/cv.h \
+ sdk/native/jni/include/opencv2/core/core_c.h \
+ sdk/native/jni/include/opencv2/core/types_c.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/assert.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/strings.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/memory.h \
+ /android-ndk-r10d/platforms/android-8/arch-arm/usr/include/math.h \
+ sdk/native/jni/include/opencv2/core/core.hpp \
+ sdk/native/jni/include/opencv2/core/version.hpp \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ sdk/native/jni/include/opencv2/core/operations.hpp \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/limits \
+ sdk/native/jni/include/opencv2/core/mat.hpp \
+ sdk/native/jni/include/opencv2/imgproc/imgproc_c.h \
+ sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ sdk/native/jni/include/opencv2/video/tracking.hpp \
+ sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ sdk/native/jni/include/opencv2/flann/defines.h \
+ sdk/native/jni/include/opencv2/flann/config.h \
+ sdk/native/jni/include/opencv2/flann/flann.hpp \
+ sdk/native/jni/include/opencv2/flann/flann_base.hpp \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cassert \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio \
+ sdk/native/jni/include/opencv2/flann/general.h \
+ sdk/native/jni/include/opencv2/flann/defines.h \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/stdexcept \
+ sdk/native/jni/include/opencv2/flann/matrix.h \
+ sdk/native/jni/include/opencv2/flann/params.h \
+ sdk/native/jni/include/opencv2/flann/any.h \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/typeinfo \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream \
+ sdk/native/jni/include/opencv2/flann/saving.h \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring \
+ sdk/native/jni/include/opencv2/flann/nn_index.h \
+ sdk/native/jni/include/opencv2/flann/result_set.h \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/set \
+ sdk/native/jni/include/opencv2/flann/all_indices.h \
+ sdk/native/jni/include/opencv2/flann/kdtree_index.h \
+ sdk/native/jni/include/opencv2/flann/dynamic_bitset.h \
+ sdk/native/jni/include/opencv2/flann/dist.h \
+ sdk/native/jni/include/opencv2/flann/heap.h \
+ sdk/native/jni/include/opencv2/flann/allocator.h \
+ sdk/native/jni/include/opencv2/flann/random.h \
+ sdk/native/jni/include/opencv2/flann/kdtree_single_index.h \
+ sdk/native/jni/include/opencv2/flann/kmeans_index.h \
+ sdk/native/jni/include/opencv2/flann/logger.h \
+ sdk/native/jni/include/opencv2/flann/composite_index.h \
+ sdk/native/jni/include/opencv2/flann/linear_index.h \
+ sdk/native/jni/include/opencv2/flann/hierarchical_clustering_index.h \
+ sdk/native/jni/include/opencv2/flann/lsh_index.h \
+ sdk/native/jni/include/opencv2/flann/lsh_table.h \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iomanip \
+ sdk/native/jni/include/opencv2/flann/autotuned_index.h \
+ sdk/native/jni/include/opencv2/flann/ground_truth.h \
+ sdk/native/jni/include/opencv2/flann/index_testing.h \
+ sdk/native/jni/include/opencv2/flann/timer.h \
+ sdk/native/jni/include/opencv2/flann/sampling.h \
+ sdk/native/jni/include/opencv2/calib3d/calib3d.hpp \
+ sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
+ sdk/native/jni/include/opencv2/legacy/compat.hpp \
+ sdk/native/jni/include/opencv2/core/internal.hpp \
+ sdk/native/jni/include/opencv2/highgui/highgui.hpp \
+ sdk/native/jni/include/opencv2/highgui/highgui_c.h
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/GLES/gl.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/GLES/glplatform.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/GLES/glext.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/pthread.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/time.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/time.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/time.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/siginfo.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/signal.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/string.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/signal.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm-generic/signal.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/sigcontext.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/ucontext.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/user.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sched.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/Math.h:
+
+sdk/native/jni/include/opencv/cv.h:
+
+sdk/native/jni/include/opencv2/core/core_c.h:
+
+sdk/native/jni/include/opencv2/core/types_c.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/math.h:
+
+sdk/native/jni/include/opencv2/core/core.hpp:
+
+sdk/native/jni/include/opencv2/core/version.hpp:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/limits:
+
+sdk/native/jni/include/opencv2/core/mat.hpp:
+
+sdk/native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+sdk/native/jni/include/opencv2/video/tracking.hpp:
+
+sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+sdk/native/jni/include/opencv2/flann/defines.h:
+
+sdk/native/jni/include/opencv2/flann/config.h:
+
+sdk/native/jni/include/opencv2/flann/flann.hpp:
+
+sdk/native/jni/include/opencv2/flann/flann_base.hpp:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cassert:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio:
+
+sdk/native/jni/include/opencv2/flann/general.h:
+
+sdk/native/jni/include/opencv2/flann/defines.h:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/stdexcept:
+
+sdk/native/jni/include/opencv2/flann/matrix.h:
+
+sdk/native/jni/include/opencv2/flann/params.h:
+
+sdk/native/jni/include/opencv2/flann/any.h:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/typeinfo:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
+
+sdk/native/jni/include/opencv2/flann/saving.h:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring:
+
+sdk/native/jni/include/opencv2/flann/nn_index.h:
+
+sdk/native/jni/include/opencv2/flann/result_set.h:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/set:
+
+sdk/native/jni/include/opencv2/flann/all_indices.h:
+
+sdk/native/jni/include/opencv2/flann/kdtree_index.h:
+
+sdk/native/jni/include/opencv2/flann/dynamic_bitset.h:
+
+sdk/native/jni/include/opencv2/flann/dist.h:
+
+sdk/native/jni/include/opencv2/flann/heap.h:
+
+sdk/native/jni/include/opencv2/flann/allocator.h:
+
+sdk/native/jni/include/opencv2/flann/random.h:
+
+sdk/native/jni/include/opencv2/flann/kdtree_single_index.h:
+
+sdk/native/jni/include/opencv2/flann/kmeans_index.h:
+
+sdk/native/jni/include/opencv2/flann/logger.h:
+
+sdk/native/jni/include/opencv2/flann/composite_index.h:
+
+sdk/native/jni/include/opencv2/flann/linear_index.h:
+
+sdk/native/jni/include/opencv2/flann/hierarchical_clustering_index.h:
+
+sdk/native/jni/include/opencv2/flann/lsh_index.h:
+
+sdk/native/jni/include/opencv2/flann/lsh_table.h:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iomanip:
+
+sdk/native/jni/include/opencv2/flann/autotuned_index.h:
+
+sdk/native/jni/include/opencv2/flann/ground_truth.h:
+
+sdk/native/jni/include/opencv2/flann/index_testing.h:
+
+sdk/native/jni/include/opencv2/flann/timer.h:
+
+sdk/native/jni/include/opencv2/flann/sampling.h:
+
+sdk/native/jni/include/opencv2/calib3d/calib3d.hpp:
+
+sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
+
+sdk/native/jni/include/opencv2/legacy/compat.hpp:
+
+sdk/native/jni/include/opencv2/core/internal.hpp:
+
+sdk/native/jni/include/opencv2/highgui/highgui.hpp:
+
+sdk/native/jni/include/opencv2/highgui/highgui_c.h:
